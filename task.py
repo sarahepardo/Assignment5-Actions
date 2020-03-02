daee4385320e1533247e5ec6dd7448531d9931aa
sarahepardo <49249529+sarahepardo@users.noreply.github.com>
2020-03-01 21:28:32 -0800

    getarea function

--- task.py
+++ task.py
@@ -1,2 +1,10 @@
+import math
+
+
 def firstrun():
     return "success"
+
+def getarea(radius):
+    area= math.pi * radius ** 2
+    return area
+
--- tests.py
+++ tests.py
@@ -1,10 +1,11 @@
 import unittest
 import task
+import math
 
 
-class TestCase(unittest.TestCase) :
+class TestCase(unittest.TestCase):
 
-    def test1(selfself):
+    def test1(self):
         expected = "success"
         self.assertEqual(expected, task.firstrun())
 
@@ -12,6 +13,14 @@ class TestCase(unittest.TestCase) :
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
 
+class TestCaseA(unittest.TestCase):
+
+    def test_getarea(self,radius):
+        self.assertEqual(radius=13, 530, "true")
+
+
+
 
 if __name__ == '__main__' :
     unittest.main()
+    test_getarea()
